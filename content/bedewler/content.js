20557d7e84cfb6b63e27b0be07ae0a9690b54e70
Karina <karinabeshimova1998@gmail.com>
2025-10-31 17:07:12 +0500

    added bedewler details

---
+++ content/bedewler/content.js
@@ -0,0 +1,51 @@
+(function () {
+    window.BEDEWLER_CONTENT = {
+        'Akhan': {
+            title: 'AKHAN',
+            text: 'Gurply we Ruhubelent diýen atlaryň neslinden bolan gyr at, 2006-njy ýylda doglan. Ol Gyşrakaryň ugruna degişli. Hormatly Prezidentimiziň gatnaşmagynda halkara atçylyk festiwallarynda, Aşgabat şäherinde 2017-nji ýylda geçirilen Ýapyk binalarda we söweş sungaty boýunça V Aziýa oýunlarynyň alawyny çarpyşa galyp ýakmaga gatnaşdy. Birleşen Arap Emirlikleriniň Arap atly bedewiniň 2016-njy ýylda 9,21 sekunda geçen 10 metr aralygyny, 2018-nji ýylda Akhan çarpyşa galyp 4,19 sekunda geçip, Ginnesiň rekordlar kitabyna girdi. Bu waka Ginnesiň rekordlar kitabynyň wekili hem-de emin bolan hanym Şeyda Subaşi Gemiýiniň şaýatlygynda geçirildi. Akhan aty dünýäniň rekordlar kitabyna girizildi.',
+            image: 'atlar_png/akhan_2.png'
+        },
+        'Dalbedew': {
+            title: 'DALBEDEW',
+            text: 'Dalbedew — güýçli hem-de ýüze çykýan çydamlylygy bilen meşhur. Ýaryş meýdançalarynda durnukly netijeler görkezýär, ýördüm-terbiýesi bilen tapawutlanýar.',
+            image: 'atlar_png/Dalbedew.png'
+        },
+        'Ýanardag': {
+            title: 'ÝANARDAG',
+            text: 'Ýanardag – mele at 1991 ý. d. Ýalkymly we Dabaraly diýen atlaryň neslinden. Fakirpälwanyň ugryna degişli. Ol 1999-njy ýylda Russiýa Federasiýasynyň paýtagty Moskwa şäherinde geçirilen Halkara gözellik sergisiniň ýeňijisi-çempiony bolýar. Şonuň ýaly-da, ol Türkmenistanda ahalteke atlarynyň arasynda 3 gezek çempion adyna eýe boldy we “Soltanyň gülü” atly ýörite kubok hem-de pul baýraklary bilen sylaglandy. Aşgabat açylyş sport toplumynda geçirilen saýlaw çapyşyklarynda birnäçe ýeňiş gazandy. Ýanardag şekilli 2000-nji ýylyň Türkmenistanyň Döwlet tugrasyna (gerbine) geçirildi.',
+            image: 'atlar_png/Yanardag.png'
+        },
+        'Gyrat': {
+            title: 'GYRAT',
+            text: 'Gyrat – gyr at 1999 ýylda doglan Garadaşyak we Gülperi diýen atlaryň neslinden. Ol meşhur Ýeliniň ugryna degişli. Gyrat „Galkynyş“ milli at üstündäki oýunlar topary bilen dünýäniň birnäçe halkara sirk festiwallarynda çykyş etdi. Bu bedew Beýik Watançylyk urşunda gazanylan ýeňşiň 65 ýyllygyna bagyşlanyp, Russiýa Federasiýasynyň paýtagty Moskwa şäherinde geçirilen dabaraly harby ýörişine gatnaşan şöhratly bedewdir.',
+            image: 'atlar_png/Gyrat.png'
+        },
+        'Parasat': {
+            title: 'PARASAT',
+            text: 'Parasat — dor at, 2012-nji ýylda doglan Pyýada we Güllendam diýen atlaryň neslinden. Ol Pereň ugruna degişli. Parassat Türkmen bedewiniň baýramy mynasybetli ýurdumyyzda guralýan dürli bäsleşiklerde üstünlikli çykyş edip, baýrakly orunlara mynasyp bolan. 2019-njy ýylda geçirilen Ahalteke atlarynyň halkara gözellik bäsleşiginiň ýeňijisi. Häzirki wagtda Türkmenistanyň Prezidentiniň ahalteke atçylyk toplumynda ösdürilip ýetişdirilýär.',
+            image: 'atlar_png/Parasat.png'
+        },
+        'Gerden': {
+            title: 'GERDEN',
+            text: 'Gerden — uzun boýunly, göwnejaý görkli at. Ýeňil hereketleri we mylaýym häsiýeti bilen ünsleri çekýär.',
+            image: 'atlar_png/Gerden.png'
+        },
+        'Gülistan': {
+            title: 'GÜLISTAN',
+            text: 'Gülistan — görk we mähir saçýan at. Bäsleşiklerde gözelligi we sazlaşykly ädimleri bilen tapawutlanýar.',
+            image: 'atlar_png/Gulistan.png'
+        },
+        'Keýmir III': {
+            title: 'KEÝMIR III',
+            text: 'Keýmir III — atçylyk taryhynda yz goýan bedewleriň biri. Güýçli beden gurluşy we berk häsiýeti bilen tanalýar.',
+            image: 'atlar_png/Keymir_III.png'
+        },
+        'Gadymyýet': {
+            title: 'GADYMYÝET',
+            text: 'Gadymyýet — adyndan mälim bolşy ýaly, taryhy gadymlara baý ilkinji nesilleriň dowamçysy. Tizlik we özboluşly görnüş bilen tapawutlanýar.',
+            image: 'atlar_png/Gadymyyet.png'
+        }
+    };
+})();
+
+
