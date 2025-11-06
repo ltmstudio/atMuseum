5bc9fa6d7a748f1f1c4b017abb605a55100a62d1
Karina <karinabeshimova1998@gmail.com>
2025-11-06 16:24:29 +0500

    changed btn styles

--- content/bedewler/content.js
+++ content/bedewler/content.js
@@ -25,16 +25,16 @@
             text: 'Parasat — dor at, 2012-nji ýylda doglan Pyýada we Güllendam diýen atlaryň neslinden. Ol Pereň ugruna degişli. Parassat Türkmen bedewiniň baýramy mynasybetli ýurdumyyzda guralýan dürli bäsleşiklerde üstünlikli çykyş edip, baýrakly orunlara mynasyp bolan. 2019-njy ýylda geçirilen Ahalteke atlarynyň halkara gözellik bäsleşiginiň ýeňijisi. Häzirki wagtda Türkmenistanyň Prezidentiniň ahalteke atçylyk toplumynda ösdürilip ýetişdirilýär.',
             image: 'atlar_png/Parasat.png'
         },
-        'Gerden': {
-            title: 'GERDEN',
-            text: 'Gerden — uzun boýunly, göwnejaý görkli at. Ýeňil hereketleri we mylaýym häsiýeti bilen ünsleri çekýär.',
-            image: 'atlar_png/Gerden.png'
-        },
-        // 'Gülistan': {
-        //     title: 'GÜLISTAN',
-        //     text: 'Gülistan — görk we mähir saçýan at. Bäsleşiklerde gözelligi we sazlaşykly ädimleri bilen tapawutlanýar.',
-        //     image: 'atlar_png/Gulistan.png'
+        // 'Gerden': {
+        //     title: 'GERDEN',
+        //     text: 'Gerden — uzun boýunly, göwnejaý görkli at. Ýeňil hereketleri we mylaýym häsiýeti bilen ünsleri çekýär.',
+        //     image: 'atlar_png/Gerden.png'
         // },
+        'Gülistan': {
+            title: 'GÜLISTAN',
+            text: 'Gülistan — Akhalteke atlarynyň iň meşhur we gymmatly wekillerinden biridir. Ol 1965-nji ýylda Türkmenistan, Kokand konzeawodynda doguldy we Kaplan liniýasynyň ilkinji (ýoki birinji) ogullarynyň biri bolup, bu liniýanyň esaslandyryjysydyr. Bu gara ýürekli erkek at, öz ýokary bahalary bilen tanalyp, elit at hökmünde tanaldy. Onuň bahasy 9:10:8:7 diýip baha berlip, boýy 168 sm bolup, Akhalteke aty üçin möhüm ösüşdir. Gülistan, öz gezeginde, sport başarnyklary we ýokary derejeli hereketleri bilen meşhur bolup, bu ugurda uly üstünliklere ýetdi. Türkmenistandaky başarnykly sport karýerasyndan soň, Gülistan Latwiýa geçirilip, ol ýerde sport atlary üçin tohumçylykda ulanyldy. Ilki bilen, ol hoppa we egerme degişlilikde çykyş etdi, soňra bolsa, esasy sport görnüşi hökmünde ähli ýokary derejelere ýetdi. Gülistan, öz ussat hereketleri we ýokary hoppa başarnyklary bilen tanaldy, şonuň üçin ol soňky nesillerine-de bu ugurda üstünlikleri ýetirdi. Gülistan, Belarusda Trakenenler bilen işlenip, ondan soňky nesillerinde öz beýikligini dowam etdirdi. Onuň galyndylary, Trakenenleriň neslinde birnäçe gyzlary arkaly dowam etdi. Gülistan, diňe bir ýokary derejeli sportçy däl, eýsem uly tohumçylyk başarnyklaryna eýe bolan we Akhalteke atlarynyň taryhynda öz ýerini alan atdyr. Onuň nesilleri häzirki wagtda hem, onuň atyndan doly peýdalanyp, sportda we gözellikde uly üstünlikler gazanýarlar. 2007-nji ýylda Gülistan\'dan ilkinji çyzgynly tohum, gara çopançy at Guli-Star, doguldy. 2013-nji ýylda onuň başga bir nesli, bulanyň çopançy at Gul-Oylyk, Germaniýada Ýewropa Çempiony boldy. Gülistan, Akhalteke atlarynyň taryhynda aýratyn orun tutan we nesillerine ýokary derejeli sport we gözellik başarnyklaryny öwredip, bu atyň beýikligini dowam etdirmäge dowam edýär.',
+            image: 'atlar_png/Gulistan.png'
+        },
         // 'Keýmir III': {
         //     title: 'KEÝMIR III',
         //     text: 'Keýmir III — atçylyk taryhynda yz goýan bedewleriň biri. Güýçli beden gurluşy we berk häsiýeti bilen tanalýar.',
